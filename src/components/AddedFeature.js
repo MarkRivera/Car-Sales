c742e65a8cb99f61305eea6405c68622665c74eb
Mark Rivera <Mriver1991@gmail.com>
2020-05-22 21:18:05 -0400

    added actions, moved reducers and finished removal reducer function

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { removeFeature } from "../actions";
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    props.removeFeature(props.feature);
+  };
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={(e) => handleClick(e)}>
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
